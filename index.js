b93bbec4073ff1e59e7b5b2cf3bdff224051395a
merrymustard <merryymustard@gmail.com>
2020-01-29 11:57:05 -0600

    update0.1

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
+const express = require('express')
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const hbs = require('hbs')
 
+const app = express();
+app.use(express.static(`${__dirname}/public`));
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "hbs");
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost:127.0.0.1/recipeApp', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
-Recipe.create(data[0])
-console.log(Recipe.title)
-Recipe.createMany(data)
+  const mainRoutes = require("./routes");
+  app.use("/", mainRoutes);
+
+  process.on('SIGINT', function() {
+    mongoose.connection.close(function () {
+      console.log('Disconnect Mongoose');
+      process.exit(0);
+    });
+  });
+app.listen(3000, () => console.log("http://localhost:3000"));
+
+
